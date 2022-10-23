4c03462fda14cc4902e8951fa773f886e30232d7
fe2dback <xnqppskxn@naver.com>
2022-10-23 22:32:10 +0900

    1023

---
+++ Assets/Script/MiniGame/falp/BuildSetting.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildSetting : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        Screen.SetResolution(600, 1920, true);
+
+    }
+
+   
+}
